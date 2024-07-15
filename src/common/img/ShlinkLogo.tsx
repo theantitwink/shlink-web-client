dd4c13844b21304abd0a2d5a5272ce763d11a386
David Gerard <david@iamtheantitwink.xyz>
2024-07-15 16:22:28 -0400

    I don't need the CSS any more!

--- src/common/img/ShlinkLogo.tsx
+++ src/common/img/ShlinkLogo.tsx
@@ -7,10 +7,7 @@ export interface ShlinkLogoProps {
 
 export const ShlinkLogo = ({ color = MAIN_COLOR, className }: ShlinkLogoProps) => (
   <div className={`${className}`}>
-    <div className="bulge-container">
-      <img src="./src/common/img/butt.png" alt="Bulging Outline Image" className="bulge-image first-image" />
-      <img src="./src/common/img/link.gif" alt="Bulging Outline Image" className="bulge-image second-image" />
-    </div>
+    <img src="/icons/icon-1024x1024.png" alt="Bulging Outline Image" />
   </div>
 );
 

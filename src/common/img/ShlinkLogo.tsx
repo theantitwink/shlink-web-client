626391f1cc8dff439b6d3814188c34032666a51d
The Antitwink™️ <me@iamtheantitwink.xyz>
2024-10-04 20:00:55 -0400

    Update ShlinkLogo.tsx

--- src/common/img/ShlinkLogo.tsx
+++ src/common/img/ShlinkLogo.tsx
@@ -8,7 +8,7 @@ export interface ShlinkLogoProps {
 
 export const ShlinkLogo = ({ color = MAIN_COLOR, className, height = 24 }: ShlinkLogoProps) => (
   <div className={`${className}`} style={{display: 'inline-block'}}>
-    <img src={`/icons/icon-${height}x${height}.png`} alt="My-Short.link" />
+    <img src={`/icons/icon-${height}x${height}.png`} alt="My-Short.link" style={{ borderRadius: '15px', boxShadow: '0 0 15px rgba(0, 0, 0, 0.5)' }} />
   </div>
 );
 
